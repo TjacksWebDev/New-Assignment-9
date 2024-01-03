712bfbd0c224348a6fca93377367954c2c955119
Ty Jackson <143833425+TjacksWebDev@users.noreply.github.com>
2024-01-03 10:15:12 -0500

    init commit

---
+++ new_Assign_9/src/main/java/com/coderscampus/Assignment9/repository/Recipe.java
@@ -0,0 +1,125 @@
+package com.coderscampus.Assignment9.repository;
+public class Recipe {
+
+	private Boolean dairyFree;
+	private Boolean glutenFree;
+	private Boolean vegan;
+	private Boolean vegetarian;
+	private String title;
+	private Double spoonacularScore;
+	private Integer servings;
+	private Double pricePerServing;
+	private Double preparationMinutes;
+	private Integer cookingMinutes;
+    private Integer readyInMinutes;
+    private String instructions;
+	
+    
+    
+    
+	public Recipe(String title, Double spoonacularScore, Integer servings, Double pricePerServing,  
+			 Double preparationMinutes, Integer cookingMinutes, Integer readyInMinutes, String instructions) {
+		super();
+		this.title = title;
+		this.spoonacularScore = spoonacularScore;
+		this.servings = servings;
+		this.pricePerServing = pricePerServing;
+		this.preparationMinutes = preparationMinutes;
+		this.cookingMinutes = cookingMinutes;
+		this.readyInMinutes = readyInMinutes;
+		this.instructions = instructions;
+	}
+	
+	
+
+
+	@Override
+	public String toString() {
+		return "Recipe [dairyFree=" + dairyFree + ", glutenFree=" + glutenFree + ", vegan=" + vegan + ", vegetarian="
+				+ vegetarian + ", title=" + title + ", spoonacularScore=" + spoonacularScore + ", servings=" + servings
+				+ ", pricePerServing=" + pricePerServing + ", preparationMinutes=" + preparationMinutes
+				+ ", cookingMinutes=" + cookingMinutes + ", readyInMinutes=" + readyInMinutes + ", instructions="
+				+ instructions + "]";
+	}
+
+
+
+
+	public Integer getCookingMinutes() {
+		return cookingMinutes;
+	}
+	public Boolean getDairyFree() {
+		return dairyFree;
+	}
+	public Boolean getGlutenFree() {
+		return glutenFree;
+	}
+	public String getInstructions() {
+		return instructions;
+	}
+	public Double getPreparationMinutes() {
+		return preparationMinutes;
+	}
+	public Double getPricePerServing() {
+		return pricePerServing;
+	}
+	public Integer getReadyInMinutes() {
+		return readyInMinutes;
+	}
+	public Integer getServings() {
+		return servings;
+	}
+	public Double getSpoonacularScore() {
+		return spoonacularScore;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public Boolean getVegan() {
+		return vegan;
+	}
+	public Boolean getVegetarian() {
+		return vegetarian;
+	}
+	
+	
+	
+	
+	public void setCookingMinutes(Integer cookingMinutes) {
+		this.cookingMinutes = cookingMinutes;
+	}
+	public void setDairyFree(Boolean dairyFree) {
+		this.dairyFree = dairyFree;
+	}
+	public void setGlutenFree(Boolean glutenFree) {
+		this.glutenFree = glutenFree;
+	}
+	public void setInstructions(String instructions) {
+		this.instructions = instructions;
+	}
+	public void setPreparationMinutes(Double preparationMinutes) {
+		this.preparationMinutes = preparationMinutes;
+	}
+	public void setPricePerServing(Double pricePerServing) {
+		this.pricePerServing = pricePerServing;
+	}
+	public void setReadyInMinutes(Integer readyInMinutes) {
+		this.readyInMinutes = readyInMinutes;
+	}
+	public void setServings(Integer servings) {
+		this.servings = servings;
+	}
+	public void setSpoonacularScore(Double spoonacularScore) {
+		this.spoonacularScore = spoonacularScore;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setVegan(Boolean vegan) {
+		this.vegan = vegan;
+	}
+	public void setVegetarian(Boolean vegetarian) {
+		this.vegetarian = vegetarian;
+	}
+    
+}
---
+++ new_Assign_9/src/main/java/com/coderscampus/Assignment9/service/FileService.java
@@ -0,0 +1,120 @@
+package com.coderscampus.Assignment9.service;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+
+import com.coderscampus.Assignment9.repository.Recipe;
+@Service
+public class FileService {
+//Ingest a recipe.txt file and store this information in a Java Collection
+	
+	private String filename = "recipes.txt";
+	
+
+	
+	public FileService() {
+		
+	}
+	
+	public List<Recipe> readFile (String filename) throws IOException {
+		FileReader in = new FileReader(filename);
+		List<Recipe> recipes = new ArrayList<>();
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withEscape('\\').withIgnoreSurroundingSpaces().withFirstRecordAsHeader().parse(in);
+		for (CSVRecord record : records) {
+			
+			Boolean dairy = Boolean.parseBoolean(record.get(1));
+			Boolean gluten = Boolean.parseBoolean(record.get(2));
+			Boolean vegan	= Boolean.parseBoolean(record.get(10));
+			Boolean vegetarian	= Boolean.parseBoolean(record.get(11));
+			
+			String title	= record.get(9);
+			Double score	= Double.parseDouble(record.get(8));
+			Integer servings	= Integer.parseInt(record.get(7));
+			Double priceServ	= Double.parseDouble(record.get(5));
+			Double prepMins	= Double.parseDouble(record.get(4));
+			Integer cookingMins = Integer.parseInt(record.get(0)) ;
+			Integer readyMins	= Integer.parseInt(record.get(6));
+			String instructions	= record.get(3);
+			
+			Recipe recipe = new Recipe(title, score, servings, priceServ, prepMins, cookingMins, readyMins, instructions);
+			recipe.setDairyFree(dairy);
+			recipe.setGlutenFree(gluten);
+			recipe.setVegan(vegan);
+			recipe.setVegetarian(vegetarian);
+
+			recipes.add(recipe);
+		}	
+		return recipes;
+	}
+	
+	public List<Recipe> allRecipes() throws IOException  {
+		readFile(filename);
+		return readFile(filename);
+	}
+	
+	public List<Recipe> dairyFree() throws IOException {
+		List<Recipe> allRecipes = readFile(filename);
+		List<Recipe> dairyFreeRecipes = new ArrayList<>();
+		
+		for (Recipe recipe : allRecipes) {
+			if (recipe.getDairyFree() == true) {
+				dairyFreeRecipes.add(recipe);
+			}
+		}
+		return dairyFreeRecipes;
+	}
+	
+	public List<Recipe> glutenFree() throws IOException {
+		List<Recipe> allRecipes = readFile(filename);
+		List<Recipe> glutenFreeRecipes = new ArrayList<>();
+		
+		for (Recipe recipe : allRecipes) {
+			if (recipe.getGlutenFree() == true) {
+				glutenFreeRecipes.add(recipe);
+			}
+		}
+		return glutenFreeRecipes;
+	}
+	
+	public List<Recipe> veganAndGlutenFree() throws IOException {
+		List<Recipe> allRecipes = readFile(filename);
+		List<Recipe> veganAndGFRecipes = new ArrayList<>();
+		
+		for (Recipe recipe : allRecipes) {
+			if ((recipe.getVegan() == true) && (recipe.getGlutenFree() == true)) {
+				veganAndGFRecipes.add(recipe);
+			}
+		}
+		return veganAndGFRecipes;
+	}
+	
+	public List<Recipe> vegan() throws IOException {
+		List<Recipe> allRecipes = readFile(filename);
+		List<Recipe> veganRecipes = new ArrayList<>();
+		
+		for (Recipe recipe : allRecipes) {
+			if (recipe.getVegan() == true) {
+				veganRecipes.add(recipe);
+			}
+		}
+		return veganRecipes;
+	}
+	
+	public List<Recipe> vegetarian() throws IOException {
+		List<Recipe> allRecipes = readFile(filename);
+		List<Recipe> vegetarianRecipes = new ArrayList<>();
+		
+		for (Recipe recipe : allRecipes) {
+			if (recipe.getVegetarian() == true) {
+				vegetarianRecipes.add(recipe);
+			}
+		}
+		return vegetarianRecipes;
+	}
+}
